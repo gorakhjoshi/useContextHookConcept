221dc81f757c86a9c3417b73bb45419ab8bee069
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-10-24 09:48:55 +0545

    Extra Credit 1 Solution: useContext

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 // useContext: simple Counter
+// 💯 create a consumer hook
 
 import * as React from 'react';
 
@@ -7,18 +8,24 @@ const CountContext = React.createContext();
 function CountProvider(props) {
   const [count, setCount] = React.useState(0);
   const value = [count, setCount];
-  // could also do it like this:
-  // const value = React.useState(0)
   return <CountContext.Provider value={value} {...props} />;
 }
 
+function useCount() {
+  const context = React.useContext(CountContext);
+  if (!context) {
+    throw new Error('useCount must be used within a CountProvider');
+  }
+  return context;
+}
+
 function CountDisplay() {
-  const [count] = React.useContext(CountContext);
+  const [count] = useCount();
   return <div>{`The current count is ${count}`}</div>;
 }
 
 function Counter() {
-  const [, setCount] = React.useContext(CountContext);
+  const [, setCount] = useCount();
   const increment = () => setCount((c) => c + 1);
   return <button onClick={increment}>Increment count</button>;
 }
